3a68935d9a4c3bfb9ec68d49e4981418db24de0c
mateuszsliwa645 <mateuszsliwa645@gmail.com>
2024-09-03 11:04:47 +0200

    Added a login layout

--- app/src/main/java/com/example/hoursappandroid/MainActivity.kt
+++ app/src/main/java/com/example/hoursappandroid/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.hoursappandroid
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -15,8 +17,14 @@ import com.example.hoursappandroid.ui.theme.HoursAppAndroidTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
+
+        val LoginTextEdit = findViewById<EditText>(R.id.idLogin)
+        val PassTextExit = findViewById<EditText>(R.id.idPassword)
+        val LoginButton = findViewById<Button>(R.id.idBtnLogin)
+            
         }
     }
-}
+
 
