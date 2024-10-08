1236679874a7a2820257378bb4d209e3d875ece1
mateuszsliwa645 <mateuszsliwa645@gmail.com>
2024-10-08 12:28:02 +0200

    Version 1.0
    Version 1.0 of this app, its working perfectly

--- app/src/main/java/com/example/hoursappandroid/ObjectsActivity.kt
+++ app/src/main/java/com/example/hoursappandroid/ObjectsActivity.kt
@@ -1,8 +1,16 @@
 package com.example.hoursappandroid
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
+import android.widget.EditText
+import android.widget.RelativeLayout
+import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.android.volley.Request
@@ -10,72 +18,96 @@ import com.android.volley.RequestQueue
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import org.json.JSONException
+import org.json.JSONObject
 
-class HomePageActivity : ComponentActivity(){
 
-    lateinit var logOutBtn : Button
-    lateinit var enterOfficeBtn : Button
-    lateinit var leaveOfficeBtn : Button
-    lateinit var enterRemoteBtn : Button
-    lateinit var leaveRemoteBtn : Button
-    lateinit var objectsPageBtn : Button
+class ObjectsActivity : ComponentActivity() {
 
+    lateinit var logoutBtn : Button
+    lateinit var enterObjectBtn : Button
+    lateinit var leaveObjectBtn : Button
+    lateinit var startRoadBtn : Button
+    lateinit var endRoadBtn : Button
+    lateinit var officePageBtn : Button
+    lateinit var objectNameText : EditText
+    lateinit var objectRoadText : EditText
+    lateinit var TAG : String
+    lateinit var objectName : String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_homepage)
+        setContentView(R.layout.activity_objects)
 
-        logOutBtn = findViewById(R.id.logOutBtn)
-        enterOfficeBtn = findViewById(R.id.enterObjectBtn)
-        leaveOfficeBtn = findViewById(R.id.leaveObjectBtn)
-        enterRemoteBtn = findViewById(R.id.startRoadBtn)
-        leaveRemoteBtn = findViewById(R.id.endRoadBtn)
-        objectsPageBtn = findViewById(R.id.objectsPageBtn)
-
-
+        logoutBtn = findViewById(R.id.logOutBtn)
+        enterObjectBtn = findViewById(R.id.enterObjectBtn)
+        leaveObjectBtn = findViewById(R.id.leaveObjectBtn)
+        startRoadBtn = findViewById(R.id.startRoadBtn)
+        endRoadBtn = findViewById(R.id.endRoadBtn)
+        officePageBtn = findViewById(R.id.officePageBtn)
+        objectNameText = findViewById(R.id.ObjectEditText)
+        objectRoadText = findViewById(R.id.ObjectEditText2)
+        TAG = "My Objects Activity"
 
         val userId = intent.getStringExtra("user_id")
 
-        logOutBtn.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
+        logoutBtn.setOnClickListener {
+            intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
 
-        enterOfficeBtn.setOnClickListener {
-            userId?.let{
-                enter_office(it)
-            }
-        }
-
-        leaveOfficeBtn.setOnClickListener {
-            userId?.let{
-                leave_office(it)
-            }
-        }
-
-        enterRemoteBtn.setOnClickListener {
-            userId?.let {
-                enter_remote(it)
-            }
-        }
-
-        leaveRemoteBtn.setOnClickListener {
-            userId?.let {
-                leave_remote(it)
-            }
-        }
-
-        objectsPageBtn.setOnClickListener {
-            intent = Intent(this, ObjectsActivity::class.java)
+        officePageBtn.setOnClickListener {
+            intent = Intent(this, HomePageActivity::class.java)
             intent.putExtra("user_id", userId)
             startActivity(intent)
             finish()
         }
+
+        enterObjectBtn.setOnClickListener {
+            userId?.let {
+                Log.d(TAG,objectNameText.text.toString())
+                enter_object(it)
+            }
+        }
+
+        leaveObjectBtn.setOnClickListener {
+            userId?.let {
+                Log.d(TAG,objectNameText.text.toString())
+                leave_object(it)
+            }
+        }
+
+        startRoadBtn.setOnClickListener {
+            userId?.let {
+                Log.d(TAG,objectRoadText.text.toString())
+                start_road(it)
+            }
+        }
+
+        endRoadBtn.setOnClickListener {
+            userId?.let {
+                Log.d(TAG,objectRoadText.text.toString())
+                end_road(it)
+            }
+        }
+
+        val mainLayout = findViewById<RelativeLayout>(R.id.object_layout)
+        mainLayout.setOnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                hideKeyboard(v)
+                v.clearFocus()
+            }
+            false
+        }
     }
 
-    private fun enter_office(user_id: String) {
-        val url = "https://hosting2480966.online.pro/installatorapp/HoursApp/office_enter.php"
+    private fun hideKeyboard(view: View) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 
+    private fun enter_object(user_id: String) {
+        val url = "https://hosting2480966.online.pro/installatorapp/HoursApp/object_enter.php"
         val requestQueue: RequestQueue = Volley.newRequestQueue(this)
 
         val stringRequest = object : StringRequest(
@@ -93,43 +125,15 @@ class HomePageActivity : ComponentActivity(){
                 // Parametry do wysłania w zapytaniu POST
                 val params = HashMap<String, String>()
                 params["user_id"] = user_id
+                params["object_name"] = objectNameText.text.toString()
                 return params
             }
         }
         requestQueue.add(stringRequest)
     }
 
-
-    private fun leave_office(user_id : String){
-        val url = "https://hosting2480966.online.pro/installatorapp/HoursApp/office_leave.php"
-
-        val requestQueue: RequestQueue = Volley.newRequestQueue(this)
-
-        val stringRequest = object : StringRequest(
-            Request.Method.POST, url,
-            Response.Listener { response ->
-                // Obsługa odpowiedzi serwera
-                Toast.makeText(this, "Response: $response", Toast.LENGTH_LONG).show()
-            },
-            Response.ErrorListener { error ->
-                // Obsługa błędów
-                Toast.makeText(this, "Error: ${error.message}", Toast.LENGTH_LONG).show()
-            }
-        ) {
-            override fun getParams(): Map<String, String> {
-                // Parametry do wysłania w zapytaniu POST
-                val params = HashMap<String, String>()
-                params["user_id"] = user_id
-                return params
-            }
-        }
-        requestQueue.add(stringRequest)
-
-    }
-
-
-    private fun enter_remote(user_id : String){
-        val url = "https://hosting2480966.online.pro/installatorapp/HoursApp/remote_enter.php"
+    private fun leave_object(user_id: String) {
+        val url = "https://hosting2480966.online.pro/installatorapp/HoursApp/object_leave.php"
 
         val requestQueue: RequestQueue = Volley.newRequestQueue(this)
 
@@ -148,14 +152,15 @@ class HomePageActivity : ComponentActivity(){
                 // Parametry do wysłania w zapytaniu POST
                 val params = HashMap<String, String>()
                 params["user_id"] = user_id
+                params["object_name"] = objectNameText.text.toString()
                 return params
             }
         }
         requestQueue.add(stringRequest)
     }
 
-    private fun leave_remote(user_id : String){
-        val url = "https://hosting2480966.online.pro/installatorapp/HoursApp/remote_leave.php"
+    private fun start_road(user_id: String) {
+        val url = "https://hosting2480966.online.pro/installatorapp/HoursApp/road_enter.php"
 
         val requestQueue: RequestQueue = Volley.newRequestQueue(this)
 
@@ -174,6 +179,34 @@ class HomePageActivity : ComponentActivity(){
                 // Parametry do wysłania w zapytaniu POST
                 val params = HashMap<String, String>()
                 params["user_id"] = user_id
+                params["object_name"] = objectRoadText.text.toString()
+                return params
+            }
+        }
+        requestQueue.add(stringRequest)
+    }
+
+    private fun end_road(user_id: String) {
+        val url = "https://hosting2480966.online.pro/installatorapp/HoursApp/road_leave.php"
+
+        val requestQueue: RequestQueue = Volley.newRequestQueue(this)
+
+        val stringRequest = object : StringRequest(
+            Request.Method.POST, url,
+            Response.Listener { response ->
+                // Obsługa odpowiedzi serwera
+                Toast.makeText(this, "Response: $response", Toast.LENGTH_LONG).show()
+            },
+            Response.ErrorListener { error ->
+                // Obsługa błędów
+                Toast.makeText(this, "Error: ${error.message}", Toast.LENGTH_LONG).show()
+            }
+        ) {
+            override fun getParams(): Map<String, String> {
+                // Parametry do wysłania w zapytaniu POST
+                val params = HashMap<String, String>()
+                params["user_id"] = user_id
+                params["object_name"] = objectRoadText.text.toString()
                 return params
             }
         }
